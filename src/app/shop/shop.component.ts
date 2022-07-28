384db093870a1f6527cb45c359cceea4b6413fab
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-28 09:32:53 -0700

    get product brands/types and rendered in the view

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from '../core/models/pagination';
 import { IProduct } from '../core/models/product';
+import { IProductBrand } from '../core/models/productBrand';
+import { IProductType } from '../core/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,13 +11,39 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[] = [];
+  productBrands: IProductBrand[] = [];
+  productTypes: IProductType[] = [];
 
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getProductBrands();
+    this.getProductTypes();
+  }
+
+  getProducts(): void {
     this.shopService.getProducts().subscribe({
       next: (response) => {
         this.products = response.data;
+      },
+      error: (error) => console.error(error),
+    });
+  }
+
+  getProductBrands(): void {
+    this.shopService.getProductBrands().subscribe({
+      next: (response) => {
+        this.productBrands = response;
+      },
+      error: (error) => console.error(error),
+    });
+  }
+
+  getProductTypes(): void {
+    this.shopService.getProductTypes().subscribe({
+      next: (response) => {
+        this.productTypes = response;
       },
       error: (error) => console.error(error),
     });
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IPagination } from '../core/models/pagination';
+import { IProductBrand } from '../core/models/productBrand';
+import { IProductType } from '../core/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,13 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<IPagination>(`${this.api}/products`);
+  }
+
+  getProductBrands() {
+    return this.http.get<IProductBrand[]>(`${this.api}/product_brands`);
+  }
+
+  getProductTypes() {
+    return this.http.get<IProductType[]>(`${this.api}/product_types`);
   }
 }
