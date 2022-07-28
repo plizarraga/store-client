2a4e3b8b7fafa4f3b059634bcc2d87ebcff2661c
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-28 09:19:23 -0700

    create product brand and product type interfaces

--- src/app/core/models/product.ts
+++ src/app/core/models/product.ts
@@ -1,14 +1,12 @@
+import { IProductBrand } from "./productBrand";
+import { IProductType } from "./productType";
+
 export interface IProduct {
   id: number;
   name: string;
   description: string;
   price: string;
   pictureUrl: string;
-  productType: IProductTypeBrand;
-  productBrand: IProductTypeBrand;
-}
-
-export interface IProductTypeBrand {
-  id: number;
-  name: string;
+  productType: IProductType;
+  productBrand: IProductBrand;
 }
