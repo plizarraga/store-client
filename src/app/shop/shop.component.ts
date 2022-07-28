5ae1a38593359c4001447b7e92b295ee7e142c0f
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-28 12:12:11 -0700

    adding sort and filter by brand and category

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -13,6 +13,17 @@ export class ShopComponent implements OnInit {
   products: IProduct[] = [];
   productBrands: IProductBrand[] = [];
   productTypes: IProductType[] = [];
+  
+  productBrandIdSelected: number = 0;
+  productTypeIdSelected: number = 0;
+  sortSelected: string = 'nameAsc';
+  
+  sortOptions = [
+    { name: 'Name: A-Z', value: 'nameAsc' },
+    { name: 'Name: Z-A', value: 'nameDesc' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'priceDesc' },
+  ];
 
   constructor(private shopService: ShopService) {}
 
@@ -23,18 +34,24 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts(): void {
-    this.shopService.getProducts().subscribe({
-      next: (response) => {
-        this.products = response.data;
-      },
-      error: (error) => console.error(error),
-    });
+    this.shopService
+      .getProducts(
+        this.productBrandIdSelected,
+        this.productTypeIdSelected,
+        this.sortSelected
+      )
+      .subscribe({
+        next: (response) => {
+          this.products = response.data;
+        },
+        error: (error) => console.error(error),
+      });
   }
 
   getProductBrands(): void {
     this.shopService.getProductBrands().subscribe({
       next: (response) => {
-        this.productBrands = response;
+        this.productBrands = [{ id: 0, name: 'All' }, ...response];
       },
       error: (error) => console.error(error),
     });
@@ -43,9 +60,24 @@ export class ShopComponent implements OnInit {
   getProductTypes(): void {
     this.shopService.getProductTypes().subscribe({
       next: (response) => {
-        this.productTypes = response;
+        this.productTypes = [{ id: 0, name: 'All' }, ...response];
       },
       error: (error) => console.error(error),
     });
+  }
+
+  onProductBrandSelected(brandId: number): void {
+    this.productBrandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onProductTypeSelected(typeId: number): void {
+    this.productTypeIdSelected = typeId;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string): void {
+    this.sortSelected = sort;
+    this.getProducts();
   }
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,21 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { IPagination } from '../core/models/pagination';
 import { IProductBrand } from '../core/models/productBrand';
 import { IProductType } from '../core/models/productType';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
-
   private api = environment.api;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getProducts() {
-    return this.http.get<IPagination>(`${this.api}/products`);
+  getProducts(productBrandId?: number, productTypeId?: number, sort?: string) {
+    let params = new HttpParams();
+
+    if (productBrandId) {
+      params = params.append('brandId', productBrandId.toString());
+    }
+
+    if (productTypeId) {
+      params = params.append('typeId', productTypeId.toString());
+    }
+
+    if (sort) {
+      params = params.append('sort', sort.toString());
+    }
+    
+    return this.http.get<IPagination>(`${this.api}/products`, { observe: 'response', params: params })
+      .pipe(
+        map(response => response.body)
+      )
   }
 
   getProductBrands() {
