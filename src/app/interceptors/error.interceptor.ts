a0c053b3b6b004be8234e51d9d6b1abdf959437d
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-30 15:12:48 -0700

    added toastr service

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -6,11 +6,12 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -19,11 +20,22 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error) => {
         if (error) {
+          if (error.status === 400) {
+            if (error.error.errors) {
+              return throwError(() => new Error(error.error));
+            } else {
+              this.toastr.error(error.error.message, error.status);
+            }
+          }
+          if (error.status === 401) {
+            this.toastr.error(error.error.message, error.status);
+          }
           if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
           if (error.status === 500) {
-            this.router.navigateByUrl('/server-error');
+            const navigationExtras: NavigationExtras = {state: {error: error.error}}
+            this.router.navigateByUrl('/server-error', navigationExtras);
           }
 
           return throwError(() => new Error(error));
