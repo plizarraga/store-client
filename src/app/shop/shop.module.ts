2558fe4dc6874e97e439ceec4ea9c9f2c6510fde
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-29 21:10:04 -0700

    created home module and component, created product details component

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ShopComponent } from './shop.component';
-import { ProductItemComponent } from './product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
     SharedModule
