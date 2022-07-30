cf5b5b48d5d58a374f4a83a6130d5b3c9370f50d
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-30 09:05:24 -0700

    deleted unnecessary imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
