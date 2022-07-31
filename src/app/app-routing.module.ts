ee6c4f5ef5d577c0e828b2456c9c8dee7c2d0623
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-30 19:26:41 -0700

    styled breadcrumb component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,22 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) },
-  { path: 'not-found', component: NotFoundComponent },
-  { path: 'server-error', component: ServerErrorComponent },
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
+    data: { breadcrumb: 'Shop' },
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not Found' },
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+
+import { ToastrModule } from 'ngx-toastr';
+import {BreadcrumbModule} from 'xng-breadcrumb';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
@@ -21,6 +24,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
+    BreadcrumbModule
   ],
   exports: [NavBarComponent, SectionHeaderComponent],
 })
