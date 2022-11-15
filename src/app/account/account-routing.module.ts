13d51e241d0f9849710a7f623693c2f2390033a6
Pedro Lizarraga <plizarraga86@gmail.com>
2022-11-14 20:27:42 -0800

    Created login, register components, account module and user, address interfaces

---
+++ src/app/account/account-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AccountRoutingModule {}
