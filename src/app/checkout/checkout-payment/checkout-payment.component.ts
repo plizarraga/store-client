b3e669b8a966b696b0d4ca287418ca92ffd4f258
Pedro Lizarraga <plizarraga86@gmail.com>
2022-12-30 20:18:32 -0800

    Delete basket after order created and payment confirmed

--- src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -97,7 +97,7 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
       const paymentResult = await this.confirmPaymentWithStripe(basket);
 
       if (paymentResult.paymentIntent) {
-        this.basketService.deleteLocalBasket();
+        this.basketService.deleteBasket(basket);
         const navigationExtras: NavigationExtras = { state: createdOrder };
         this.router.navigate(['checkout/success'], navigationExtras);
       } else {
